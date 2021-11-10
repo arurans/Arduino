2d0e06aa3492f5aa971b0f1290aa69e250546c1d
arurans <arurans@stud.ntnu.no>
2021-11-10 15:08:41 +0100

    Added code to display on OLED

--- SensorLog.ino
+++ SensorLog.ino
@@ -1,11 +1,18 @@
 /*By Aruran Sivashankar
- * Read local data from DHT11 and add it to ThingSpeak
+ * Read local data from DHT11 and display it on ThingSpeak and an OLED display
+ * 
+ * HARDWARE:
+ * ESP8266-12E
+ * DHT11
+ * 0.96 Inch I2C IIC Serial 128 x 64 OLED display
  */
-
 
 #include "DHT.h"//Read measurements from DHT11-sensor (or DHT22)
 #include <ESP8266WiFi.h>
 #include <WiFiClientSecure.h>
+//Display to OLED screen
+#include <SSD1306Wire.h>
+#include <OLEDDisplayUi.h>
 
 #define TS_ENABLE_SSL //For secure connection
 #include "ThingSpeak.h" //To export our local data to ThingSpeak
@@ -23,40 +30,41 @@
 #define writeAPI "WRITE_API FOR MY CHANNEL" //Write API for your channel
 #define FINGERPRINT "27 18 92 DD A4 26 C3 07 09 B9 7A E6 C5 21 B9 5B 48 F7 16 E1" //SHA1 fingerprint for thingspeak.com, check if outdated
 
+//Setup for displaying on OLED display
+#define SCREEN_ADDRESS 0x3c
+#define SDA_PIN D3
+#define SDC_PIN D4
+
 #define measurement_delay 30000 //Time interval between each measurement in milliseconds
 
 
 //Initilize variables
-
 float temp, humidity;//Store local data
+
 DHT dht(DHTPIN, DHTTYPE);
 WiFiClientSecure client; //Create client for ThingSpeak
+
+//To display data on OLED screen
+SSD1306Wire  display(SCREEN_ADDRESS, SDA_PIN, SDC_PIN);
+OLEDDisplayUi ui(&display);
 
 
 void setup(){
   Serial.begin(115200);
+  setupDisplay();
 
   //Connect to WiFi
   WiFi.mode(WIFI_STA);//Set WiFi-mode to station
 
   client.setFingerprint(FINGERPRINT);
-  
-  Serial.print("Connecting to (WiFi): ");
-  Serial.println(ssid);
 
   WiFi.begin(ssid, password);
 
   while(WiFi.status() != WL_CONNECTED){
-    delay(3000);
-    Serial.print("Attempting to connect to ");
-    Serial.print(ssid);
-    Serial.println("...");
+    drawWiFi(&display);
   }
-
-  Serial.println("");
-  Serial.println("");
-  Serial.print("Successfully connected to ");
-  Serial.println(ssid);
+  
+  drawWiFiSuccess(&display);
 
   //Initialize ThingSpeak
   ThingSpeak.begin(client);
@@ -69,9 +77,12 @@ void setup(){
 
 void loop(){
   //Fetch local data
-  fetch_temp_and_humidity(temp, humidity);
+  bool validData = fetch_temp_and_humidity(temp, humidity);
   //Write to ThingSpeak
-  write_to_ThingSpeak(temp, humidity);
+  if (validData){
+    displayData(&display, temp, humidity);
+    write_to_ThingSpeak(temp, humidity);
+  }
   Serial.println("-------------------------------------------------");
   delay(measurement_delay);
 }
@@ -80,30 +91,21 @@ void loop(){
 /*HELPER FUNCTIONS
  * Get local data
  * Write to ThingSpeak
+ * Display data on OLED display
  */
 
 
 //Read local data from sensors
-void fetch_temp_and_humidity(float& temp, float& humidity){
+bool fetch_temp_and_humidity(float& temp, float& humidity){
 
   temp = dht.readTemperature();//Fetch temperature from sensor in calcius
   humidity = dht.readHumidity();//Fetch relative humidity from sensor
   
   if (isnan(temp) || isnan(humidity)){//Check if reading is invalid
-    Serial.println("Invalid read from DHT11-sensor");
-    return;
+    return false;
   }
-
-  Serial.print("Temperature: ");
-  Serial.print(temp);
-  Serial.println("°C");
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.println("%");
-
-  return;
+  return true;
 }
-
 
 //Write local data to ThingSpeak
 void write_to_ThingSpeak(const float temp, const float humidity){
@@ -117,6 +119,57 @@ void write_to_ThingSpeak(const float temp, const float humidity){
     Serial.print("Error while uploading data to ThingSpeak channel. Error code: ");
     Serial.println(val);
   } 
+  
+  return;
+}
 
+//Initialize the display
+void setupDisplay(){
+  display.init();
+  display.clear();
+  display.display();
+
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setContrast(100);
+
+  return;
+}
+
+//Almost redundant function, cleaner in the setup function
+void drawWiFi(OLEDDisplay *display) {
+    display->drawString(5, 10, "Attempting to connect to ");
+    display->drawString(15, 25, ssid);
+    display->drawString(40, 25, "...");
+    display->display();
+    delay(3000);
+    display->clear();
+    
+    return;
+}
+
+//Display successful WiFi connection
+void drawWiFiSuccess(OLEDDisplay *display){
+  display->clear();
+  display->drawString(5, 10, "Successfully connected to ");
+  display->drawString(5, 30, ssid);
+  display->display();
+  delay(3000);
+  return;
+}
+
+//Display the local temperature and humidity on the OLED screen
+void displayData(OLEDDisplay *display, const float temp, const float humidity){
+  display->clear();
+  //Display temperature
+  display->drawString(5, 10, "Temp: ");
+  display->drawString(55, 10, String(temp));
+  display->drawString(85, 10, "°C");
+  //Display humidity
+  display->drawString(5, 30, "Humidity: ");
+  display->drawString(55, 30, String(humidity));
+  display->drawString(85, 30, "%");
+  display->display();
+  
   return;
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 # Arduino project
 
-Code for reading local temperature and humidity from a DHT11 sensor and logging data on ThingSpeak
+Code for reading local temperature and humidity from a DHT11 sensor and logging data on ThingSpeak. Display the data on OLED display.
+
+**TODO:**
+
+    1. Add multiple frames to the OLED display
+    2. Add support for other sensors
